6bb6b6c575f5de8faa908cebb383f3cfa1335a64
senayakagunduz <senayakgndz@gmail.com>
2023-02-18 00:10:40 +0300

    navbar changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Container } from "reactstrap";
 import "./header.css";
-import ReactWhatsapp from "react-whatsapp";
 
 const navLinks = [
   {
+    id:1,
     display: "Home",
     url: "#home",
+    
   },
   {
+    id:2,
     display: "About",
     url: "#about",
   },
   {
+    id:3,
     display: "Services",
     url: "#services",
   },
   {
+    id:4,
     display: "Portfolio",
     url: "#portfolio",
   },
   {
+    id:5,
     display: "Contact",
     url: "#contact",
   },
@@ -28,38 +32,32 @@ const navLinks = [
 
 const Header = () => {
   return (
-    <header className="header">
-      <Container>
-        <div className="navigation d-flex align-items-center justify-content-between sticky-top">
-          <div className="logo">
-            <h5>Şenay</h5>
-          </div>
-          <div className="nav__menu">
-            <ul className="nav__list">
-              {navLinks.map((item, index) => (
-                <li className="nav__item" key={index}>
-                  <a href={item.url} rel="noreferrer" >{item.display}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="nav__right d-flex align-items-center gap-4">
-            {/* <ReactWhatsapp
-              number="+90-539-663-7014"
-              message="Hello World!!!"
-              target="_blank"
-              style={{ background: "#0E1B31", border: "none" }}
-            >
-             <button className="btn"> Let's Talk</button>
-            </ReactWhatsapp> */}
 
-            <span className="mobile__menu">
-              <i className="ri-menu-5-line"></i>
-            </span>
-          </div>
+    <nav className="navbar navbar-expand-lg  ml-2 mr-2 " style={{ backgroundColor: "#7865FF" }}>
+      <div className="container">
+        <a className="navbar-brand brand" href="/#">Şenay</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-around nav__menu" id="navbarSupportedContent">
+          <ul className="navbar-nav nav__list ">
+            {
+              navLinks.map((link) => {
+                const { display, url,id } = link;
+                return (
+                  <li key={id} className="nav-item links ">
+                    <a className="nav-link  brand" rel="noreferrer" href={url}>{display}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
         </div>
-      </Container>
-    </header>
+      </div>
+
+    </nav>
+
+
   );
 };
 
