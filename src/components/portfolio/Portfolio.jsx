bb442a181e4da81329596579158a9c2a0c18edb4
senayakagunduz <senayakgndz@gmail.com>
2023-02-17 04:29:46 +0300

    png added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./portfolio.css";
 import { Container, Row, Col } from "reactstrap";
 
-import img1 from "../../assets/img/havadurumu.jpg";
+import img1 from "../../assets/img/weatherapp.png";
 import img2 from "../../assets/img/asiankitchen.png";
 import img3 from "../../assets/img/linkedin.png";
 import img4 from "../../assets/img/medium.png";
 import img5 from "../../assets/img/tour.png";
+import img6 from "../../assets/img/dictionary-tailwind.png"
 
 const portfolioData = [
   {
     imgUrl: img1,
     title: "Weather App",
-    url: "https://senayakagunduz-weather-app.netlify.app/",
+    url: "https://senayakgndz-weatherapp-react.netlify.app/",
   },
   {
     imgUrl: img2,
@@ -34,6 +35,11 @@ const portfolioData = [
     title:"Tour Website",
     url:"https://senayakgndz-backroads-website.netlify.app/",
   },
+  {
+    imgUrl:img6,
+    title:"English Dictionary",
+    url:"https://senayakagndz-english-dict-tailwnd.netlify.app/"
+  }
 
 ];
 const Portfolio = () => {
